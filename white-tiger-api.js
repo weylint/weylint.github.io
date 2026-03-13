79eb7cab8bbde1d82346ec8f1d3737e17217fb8a
Weylin <weylinto@gmail.com>
2026-03-13 10:57:12 +0100

    Refactored API access into a shared .js

---
+++ white-tiger-api.js
@@ -0,0 +1,16 @@
+// Shared API access for the White Tiger Eco server
+// https://white-tiger.play.eco
+const WhiteTigerAPI = (() => {
+  const BASE_URL = 'https://white-tiger.play.eco/api/v1/plugins/EcoPriceCalculator';
+
+  async function fetchEndpoint(endpoint) {
+    const response = await fetch(`${BASE_URL}/${endpoint}`);
+    if (!response.ok) throw new Error(`HTTP Error (${endpoint}): ${response.status}`);
+    return response.json();
+  }
+
+  return {
+    fetchRecipes: () => fetchEndpoint('recipes'),
+    fetchStores:  () => fetchEndpoint('stores'),
+  };
+})();
